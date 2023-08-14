95dbae92ed63c4334ff423fbe6acb75b402c63f7
Zhongwei Liu <whitney.liu@mail.utoronto.ca>
2023-08-14 09:00:16 -0700

    add account page

--- client/src/components/accountPage.js
+++ client/src/components/accountPage.js
@@ -1,8 +1,32 @@
 import SideNav from "./sideNav";
+import "../styles/accountPage.scss";
+
 export default function AccountPage() {
+  const email = "123@gmail.com";
   return (
     <>
-      <SideNav currentHerf="/account"/>
+      <SideNav currentHerf="/account" />
+      <main>
+        <div className="content-container">
+          <div className="content-container-layout">
+            <ul>
+              <li className="li-text">Account Information</li>
+              <li>
+                <dl>
+                  <div className="section">
+                    <dt>Email address</dt>
+                    <dd>{email}</dd>
+                  </div>
+                  <div className="section">
+                    <dt>Password</dt>
+                    <dd>reset your password</dd>
+                  </div>
+                </dl>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
